0498b48399e25c5caeda39e37f288c81f4dc56e8
Cris <cristiantarazon01@gmail.com>
2018-04-16 10:06:58 -0700

    Added A6 and fixed A3

--- A3/script.js
+++ A3/script.js
@@ -13,15 +13,27 @@ var start = function()
   var markup = "";
   for(var i = 0; i < allImages.length; i++){
     var imgURL = allImages[i];
-     markup = markup + "<div class='slide' style='background-image:url(" + imgURL + ")'></div>";
+    markup = markup + "<div class='slide' style='background-image:url(" + imgURL + ")'></div>";
   }
+
+  /*
+  var i = 0;
+  while(i < allImages.length){
+    var imgURL = allImages[i];
+    markup = markup + "<div class='slide' style='background-image:url(" + imgURL + ")'></div>";
+    i = i + 1;
+  }
+   */
   $("#ssContainer").html(markup);
- 
- var markup1 = "";
+
+  var markup1 = "";
   for(var i = 0; i < allImages.length; i++){
     markup1 = markup1 + '<button onclick="goToSlide(' + (i + 1) + ',1000)">' + (i + 1) + '</button>';
   }
   $("#numContainer").html(markup1);
+
+
+
   goToSlide(1, 0);
 }
 
@@ -29,26 +41,30 @@ var start = function()
 var goToSlide = function(n, d)
 {
   d = d || 0;
-  $("#ssContainer .slide").stop().fadeOut(d);
-  $("#ssContainer .slide:nth-of-type(" + n + ")").animate({"margin-left": "100%" , opacity:0},0).fadeIn(0).animate({"margin-left":0, opacity:1});
+  $(".slide").stop().removeClass("currentSlide").removeClass("slideLeft").removeClass("slideRight");
 
-  $("#numContainer button").removeClass("active");
-  $("#numContainer button:nth-of-type(" + n + ")").addClass("active");
+  $(".slide:nth-of-type(" + n + ")").addClass("currentSlide");
+  $(".currentSlide").animate({left: "0%"}, 500); 
+  var z = 0
+  while (z < n) {
+    $(".slide:nth-of-type(" + z + ")").addClass("slideLeft");
+    z++;
+  }
+  $(".slideLeft").animate({left: "-100%"}, 500);
+  var x = 0
+  x = n + 1
+  while (x < (allImages.length + 1)) {
+    $(".slide:nth-of-type(" + x + ")").addClass("slideRight");
+    x++;
+  }
+  $(".slideRight").animate({left: "100%"}, 500); 
 
-  currentSlide = n;
-}
-var goToSlide2 = function(n, d)	
-{
-  d = d || 0;
-  $("#ssContainer .slide").stop().fadeOut(d);
-  $("#ssContainer .slide:nth-of-type(" + n + ")").animate({"margin-left": "-100%" , opacity:0},0).fadeIn(0).animate({"margin-left":0, opacity:1})
+    $("#numContainer button").removeClass("active");
+    $("#numContainer button:nth-of-type(" + n + ")").addClass("active");
 
-  $("#numContainer button").removeClass("active");
-  $("#numContainer button:nth-of-type(" + n + ")").addClass("active");
-
-  currentSlide = n;
-}
-
+    currentSlide = n;
+  }
+ 
 
 
 var goNext = function()
@@ -57,7 +73,7 @@ var goNext = function()
   if (n > allImages.length){
     n = 1;
   } 
-  goToSlide(n, 100);
+  goToSlide(n, 1000);
 }
 
 var goPrev = function()
@@ -66,5 +82,6 @@ var goPrev = function()
   if (n < 1){
     n = allImages.length;
   } 
-  goToSlide2(n, 100);
+  
+  goToSlide(n, 1000);
 }
